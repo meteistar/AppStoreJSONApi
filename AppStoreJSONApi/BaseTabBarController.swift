ffd075089f8972d63633239bdaf55167bab599eb
meteistar <gargamel_1987@hotmail.com>
2023-04-29 23:54:05 +0300

    Added tabor controller and navbar

--- AppStoreJSONApi/BaseTabBarController.swift
+++ AppStoreJSONApi/BaseTabBarController.swift
@@ -12,6 +12,30 @@ class BaseTabBarController: UITabBarController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .yellow
+//        view.backgroundColor = .yellow
+        let redViewController = UIViewController()
+        redViewController.navigationItem.title = "Apps"
+        redViewController.view.backgroundColor = .white
+        
+        let redNavController = UINavigationController(rootViewController: redViewController)
+        redNavController.tabBarItem.title = "Apps"
+        redNavController.tabBarItem.image = #imageLiteral(resourceName: "apps")
+        redNavController.navigationBar.prefersLargeTitles = true
+        redNavController.navigationBar.backgroundColor = .white
+        
+        let blueViewController = UIViewController()
+        blueViewController.navigationItem.title = "Search"
+        blueViewController.view.backgroundColor = .white
+        
+        let blueNavController = UINavigationController(rootViewController: blueViewController)
+        blueNavController.tabBarItem.title = "Search"
+        blueNavController.tabBarItem.image = #imageLiteral(resourceName: "search")
+        blueNavController.navigationBar.prefersLargeTitles = true
+        blueNavController.navigationBar.backgroundColor = .white
+        
+        viewControllers = [
+            redNavController,
+            blueNavController
+        ]
     }
 }
