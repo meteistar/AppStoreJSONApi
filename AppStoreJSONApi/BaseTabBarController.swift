50833b340cee81564fdb9f3ded2f32c4a1134a05
meteistar <gargamel_1987@hotmail.com>
2023-04-29 21:32:45 +0300

    Initial commit

---
+++ AppStoreJSONApi/BaseTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseTabBarController.swift
+//  AppStoreJSONApi
+//
+//  Created by mete on 29.04.2023.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+    }
+}
