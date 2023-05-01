0919a15ee73a0785adb96c93d7542b43611c0963
meteistar <gargamel_1987@hotmail.com>
2023-05-01 22:26:31 +0300

    Added red collection view cells to Search view controller

--- AppStoreJSONApi/Controller/AppsSearchController.swift
+++ AppStoreJSONApi/Controller/AppsSearchController.swift
@@ -7,14 +7,29 @@
 
 import UIKit
 
-class AppsSearchController: UICollectionViewController {
+class AppsSearchController: UICollectionViewController,
+                            UICollectionViewDelegateFlowLayout {
+    
+    fileprivate let cellId = "Id1234"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView.backgroundColor = .red
+        collectionView.backgroundColor = .white
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 250)
+    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
     init() {
         super.init(collectionViewLayout: UICollectionViewFlowLayout())
     }
